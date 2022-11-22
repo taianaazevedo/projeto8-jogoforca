1ec05088bc19b3e6e54e6acf05534e03c7e33ffa
taianaazevedo <taianazev@gmail.com>
2022-11-22 12:44:23 -0300

    layout estado inicial ok

---
+++ src/Jogo.js
@@ -0,0 +1,12 @@
+export default function Jogo() {
+    return (
+        <div class="initial-state">
+            <div class="image-forca">
+                <img src="./assets/forca0.png" />
+            </div>
+            <div class="button">
+                <button>Escolher palavra</button>
+            </div>
+        </div>
+    );
+}
